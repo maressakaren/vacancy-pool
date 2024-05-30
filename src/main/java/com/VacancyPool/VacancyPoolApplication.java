c05e3dcf8c29ebb0a11e757a69da45b9b97ea83f
maressakaren <khsmaressa@gmail.com>
2024-05-30 20:29:15 -0300

    :tada: Starting the project

---
+++ src/main/java/com/VacancyPool/VacancyPoolApplication.java
@@ -0,0 +1,13 @@
+package com.VacancyPool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyPoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VacancyPool/VacancyPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VacancyPool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
